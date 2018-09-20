63beb23c0a75acb4ea17bfa574adbb9e5d5a1cdf
chufengl <chufengl@asu.edu>
2018-09-20 14:19:44 -0700

    remove duplicate

--- pattern_sim_main_saguaro_5.m
+++
@@ -1,6 +0,0 @@
-%"pattern_sim_main_saguaro.m" is to the batch script to run the pattern_sim
-%module based on two_color_randomori.m script on saguaro.
-
-two_color_randori(400,'/home/chufengl/test_folder/MOF_pat_sim/MOF_batch5',...
-    'MOF_dumb',[25.9322,25.9322,6.8365,90.00,90.00,120.00],1000,1000,1,0,9.540,9.660,1e12,0.5,1,110,0.075,50.00);
-
